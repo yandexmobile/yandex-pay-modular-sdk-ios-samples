1c23e8bc87f6349271fca353e92c3003080131fd
lroofer <lroofer@icloud.com>
2026-04-20 17:44:16 +0300

    Initial Commit

---
+++ YandexPaySDKModularSDK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YandexPaySDKModularSDK
+//
+//  Created by Egor Kolobaev on 20.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YandexPaySDKModularSDK/YandexPaySDKModularSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexPaySDKModularSDKApp.swift
+//  YandexPaySDKModularSDK
+//
+//  Created by Egor Kolobaev on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct YandexPaySDKModularSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
